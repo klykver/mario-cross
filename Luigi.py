dd2e0e839ee38257c2aabe19c5ddef64b1ff1d17
klykver <klyukver@gmail.com>
2025-11-12 19:56:37 +0100

    Ya dodav blat Luigi

---
+++ Luigi.py
@@ -0,0 +1,50 @@
+class Luigi:
+    def __init__(self, x: int, y: int, max_floor: int):
+        self._x = x
+        self._y = y
+        self._floor = 0
+        self._max_floor = max_floor
+        self._state = 'idle'  # 'idle', 'moving', 'busy'
+
+    @property
+    def x(self) -> int:
+        return self._x
+
+    @x.setter
+    def x(self, value: int):
+        if not isinstance(value, int):
+            raise TypeError("Position x must be an integer")
+        self._x = value
+
+    @property
+    def y(self) -> int:
+        return self._y
+
+    @y.setter
+    def y(self, value: int):
+        if not isinstance(value, int):
+            raise TypeError("Position y must be an integer")
+        self._y = value
+
+    @property
+    def floor(self) -> int:
+        return self._floor
+
+    @floor.setter
+    def floor(self, value: int):
+        if not isinstance(value, int):
+            raise TypeError("Floor must be an integer")
+        elif not (0 <= value <= self._max_floor):
+            raise ValueError(f"Floor must be between 0 and {self._max_floor}")
+        else:
+            self._floor = value
+
+    @property
+    def state(self) -> str:
+        return self._state
+
+    @state.setter
+    def state(self, value: str):
+        if value not in ('idle', 'moving', 'busy'):
+            raise ValueError(f"Invalid state {value}")
+        self._state = value

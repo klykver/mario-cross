e142da60a71b6421d559cd91feaba4d629fa1879
uni3maks <100583548@alumnos.uc3m.es>
2025-11-27 09:57:55 +0100

    fixing the map

--- game.py
+++ game.py
@@ -6,7 +6,7 @@ import pyxel
 
 class Game:
     def __init__(self):
-        pyxel.init(440, 280, title="Mario Bros Factory") #326
+        pyxel.init(504, 280, title="Mario Bros Factory") #326---440
         pyxel.load("assets/sprites.pyxres")
         self.mario = Mario(313, 235)
         self.packages = []
@@ -17,22 +17,22 @@ class Game:
     def draw_background(self):
         # (x, y, img, u, v, w, h)
         pyxel.blt(30,157,2,178,122,70,38,0) #truck
-        pyxel.blt(386, 180, 0, 0, 184, 24, 72,0)# pipe
-        pyxel.blt(341, 260, 0, 141, 112, 76, 10,0) # conveyor 0
+        pyxel.blt(406, 180, 0, 0, 184, 24, 72,0)# pipe
+        pyxel.blt(341, 260, 0, 141, 112, 92, 10,0) # conveyor 0
 
         pyxel.blt(313, 177, 0, 0, 72, 49, 29,0) #2º floor mario
         pyxel.blt(313, 224, 0, 0, 72, 49, 29,0) # 1º floor mario
-        pyxel.blt(313, 272, 0, 3, 168, 53, 14,0) #graund mario
+        pyxel.blt(313, 272, 0, 3, 152, 200, 14,0) #graund mario
         pyxel.blt(139, 195, 0, 63, 72, 49,29,0 ) #1ºfloor luigi
         pyxel.blt(139, 147, 0, 63, 72, 49, 29,0) #2º floor luigi
         pyxel.blt(139, 243, 0, 64, 168, 48, 39,0)  # graund luigi
 
-        pyxel.blt(0, 147, 0, 4, 152, 150, 15,0) #complementary road for truck2º
+
         pyxel.blt(0, 195, 0, 4, 152, 150, 15,0)  # complementary road for truck 1ºfloor
         pyxel.blt(0, 265, 0, 4, 152, 140, 15,0)  # complementary road for boss luigi
-        pyxel.blt(350, 177, 0, 4, 152, 150, 15,0)  # complementary road for boss mario2º
-        pyxel.blt(350, 224, 0, 4, 152, 37, 15, 0)  # complementary road connect pipe
-
+        pyxel.blt(350, 177, 0, 4, 152, 195, 15,0)  # complementary road for boss mario2º
+        pyxel.blt(350, 224, 0, 4, 152, 57, 15, 0)  # complementary road connect pipe
+        pyxel.blt(130, 147, 0, 129, 168, 15, 49) #horizontal wall luigi
 
 
 
